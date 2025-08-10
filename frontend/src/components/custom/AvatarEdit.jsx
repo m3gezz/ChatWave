5a5f59951d676f997d1057f1586d526f9838769e
m3gezz <m3gezz@gmail.com>
2025-08-10 19:21:36 +0100

    finishing the validation of the avatar editor

--- frontend/src/components/custom/AvatarEdit.jsx
+++ frontend/src/components/custom/AvatarEdit.jsx
@@ -9,11 +9,21 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { avatars } from "../../data/avatars";
+import { useForm } from "react-hook-form";
+import { useMainContext } from "../../contexts/MainContext";
 
 export default function AvatarEdit() {
+  const { user } = useMainContext();
+  const form = useForm({ defaultValues: { avatar: user.avatar } });
+  const selectedAvatar = form.watch("avatar");
+  const initial = user.username.charAt(0).toUpperCase();
+  const handleOnSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <DialogContent className="sm:max-w-[425px]">
-      <form>
+    <DialogContent className="sm:max-w-[425px] select-none">
+      <form onSubmit={form.handleSubmit(handleOnSubmit)}>
         <DialogHeader>
           <DialogTitle>Edit Avatar</DialogTitle>
           <DialogDescription>
@@ -26,15 +36,40 @@ export default function AvatarEdit() {
               <div key={index}>
                 <input
                   type="radio"
-                  name="avatar"
+                  {...form.register("avatar")}
                   id={`avatar ${index}`}
                   className="hidden"
+                  value={avatar}
                 />
                 <label htmlFor={`avatar ${index}`}>
-                  <img src={avatar} alt="" className="w-10 h-10 rounded-full" />
+                  <img
+                    src={avatar}
+                    alt=""
+                    className={`cursor-pointer w-10 h-10 rounded-full ${
+                      selectedAvatar == avatar &&
+                      "border-2 border-muted-foreground"
+                    }`}
+                  />
                 </label>
               </div>
             ))}
+            <div>
+              <input
+                type="radio"
+                {...form.register("avatar")}
+                id={`no-avatar`}
+                className="hidden"
+                value={""}
+              />
+              <label
+                htmlFor={`no-avatar`}
+                className={`cursor-pointer flex items-center justify-center bg-foreground text-background w-10 h-10 rounded-full ${
+                  selectedAvatar == "" && "border-2 border-muted-foreground"
+                }`}
+              >
+                {initial}
+              </label>
+            </div>
           </div>
         </div>
         <DialogFooter>
--- frontend/src/data/avatars.js
+++ frontend/src/data/avatars.js
@@ -1,5 +1,5 @@
 export const avatars = [
   "https://github.com/shadcn.png",
-  "https://github.com/shadcn.png",
-  "https://github.com/shadcn.png",
+  "https://github.com/evilrabbit.png",
+  "https://github.com/leerob.png",
 ];
